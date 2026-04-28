f5bfb8b55bac76b993e29aa739e742790e58011d
Anshul Joshi <anshuljoshi@Anshuls-MacBook-Air.local>
2026-04-28 23:10:16 +0530

    Make base path dynamic for GitHub and Render compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/trekhills-website/',
+  base: process.env.GITHUB_ACTIONS ? '/trekhills-website/' : '/',
   plugins: [react()],
 })
